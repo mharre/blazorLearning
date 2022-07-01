315918d89ec5b052f2cf71229cbb6578b9084b6d
unknown <matthewharre@gmail.com>
2022-07-01 11:01:30 -0700

    added starting files and first bascic component

---
+++ blazorApp/Shared/Products.cs
@@ -0,0 +1,11 @@
+﻿namespace blazorApp.Shared
+{
+    public class Products
+    {
+        public int id { get; set; }
+        public string title { get; set; } = string.Empty;
+        public string description { get; set; } = string.Empty;
+        public string imageUrl { get; set; } = string.Empty;
+        public decimal price { get; set; }
+    }
+}
